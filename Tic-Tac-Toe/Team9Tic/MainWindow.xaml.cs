b9a9c53915363799843a3a4d127c54b95798f251
MartinCarrion <martin.a.carrion-1@ou.edu>
2019-04-30 18:57:17 -0500

    Buttons

--- Tic-Tac-Toe/Team9Tic/MainWindow.xaml.cs
+++ Tic-Tac-Toe/Team9Tic/MainWindow.xaml.cs
@@ -27,11 +27,47 @@ namespace Team9Tic
             
             InitializeComponent();
             
+            
         }
 
         private void BtnStart_Click(object sender, RoutedEventArgs e)
         {
             btnStart.Visibility = Visibility.Collapsed;
+            Game();
+
+        }
+        public void Game()
+        {
+            Type = new Symbol[9];//Establishes array
+            for (int i = 0; i < Type.Length; i++)
+            {
+                Type[i] = Symbol.NoSpace;
+
+            }
+            Turns = true;
+            RowsandColumns.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = "";
+                button.Background = Brushes.Black;
+                button.Foreground = Brushes.Gray;
+
+            });
+            End = false;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (End)
+            {
+                MessageBox.Show("Game has ended");
+                Game();
+                return;
+
+            }
+            var button = (Button)sender;
+            var column = Grid.GetColumn(button);//Helps Place buttons
+            var row = Grid.GetRow(button);//Helps Place Buttons
+            var index = column + (row * 3);//equation that helps solidify buttons
         }
     }
 }
